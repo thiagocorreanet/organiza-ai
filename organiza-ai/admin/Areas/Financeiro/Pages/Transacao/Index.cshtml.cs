fd774524fdd8786968d564aa2f036da9406752d5
Thiago Botelho <thiago.botelho@globalsys.com.br>
2025-05-25 13:33:39 -0300

    Ajuste de layout

---
+++ organiza-ai/admin/Areas/Financeiro/Pages/Transacao/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace admin.Areas.Financeiro.Pages.Transacao;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
